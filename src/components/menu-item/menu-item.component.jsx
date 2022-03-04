ecfb2f98b4f6ab17933e4ef004da227ea76e476f
Tchilo <tchiloross@gmail.com>
2022-03-04 06:50:29 +0200

    Add more height to last two images using size value in directory state

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './menu-item.style.scss'
 
-export const MenuItem = ({title,imageUrl}) => (
+export const MenuItem = ({title,imageUrl,size}) => (
   <div 
   style={{
-    backgroundImage: `url({${imageUrl}})`
+    backgroundImage: `url(${imageUrl})`
   }}
-  className="menu-item">
+  className={`${size} menu-item`}>
         <div className="content">
           <h1 className="title">{title}</h1>
           <span className="subtitle">SHOP NOW</span>
